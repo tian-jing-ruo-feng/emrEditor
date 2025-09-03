6bd298838eb3b43c21746a2f1c86de71a553f045
mr.tian <2236117954@qq.com>
2025-09-03 09:48:17 +0800

    ✨ feat(utils/eventBus): 去除冗余代码

--- src/utils/eventBus.ts
+++ src/utils/eventBus.ts
@@ -1,20 +1,8 @@
 import mitt from 'mitt'
 
-/** 保存为导航节点 */
 export const EVENT_SAVE_AS_NAVIGATION = 'saveAsNavigation'
+
+/** 保存为导航节点 */
 const emitter = mitt()
-
-// listen to an event
-emitter.on('foo', e => console.log('foo', e))
-
-// listen to all events
-emitter.on('*', (type, e) => console.log(type, e))
-
-// fire an event
-emitter.emit('foo', { a: 'b' })
-
-// clearing all events
-emitter.all.clear()
-
 
 export default emitter
